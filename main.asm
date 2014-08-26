f7060b2ef25933ac39f3510642305d12c15cb93d
hang-on <asje@me.com>
2014-08-26 23:26:28 +0200

    Chest scrolling into chest section

--- main.asm
+++ main.asm
@@ -492,38 +492,8 @@ mvEast:      ld    hl, plrX        ; the horizontal pos. of player
 
 ; Scroll chest if it is on screen.
 
-             ld   a, (cstMode)     ; point to chest mode
-             cp   CHESTOFF         ; is chest turned off?
-             jp   z, +       ; if so, skip to column check
+             call  scrlCst
 
-             ld   hl, cstX         ; point to chest x pos
-             dec  (hl)             ; decrement it
-             ld   a, (hl)          ; put value in A for a comparison
-             cp   0                ; is chest x = 0 (blanked clmn)?
-             jp   nz, uptChest     ; if not, forward to update chest
-
-; Chest has scrolled off screen, so destroy it.
-
-             ld    c, 0            ; reset charcode
-             ld    d, 0            ; reset x pos
-             ld    e, 0            ; reset y pos
-             ld    b, CHESTSAT     ; B = the chest's index in SAT
-             call  goSprite        ; update SAT RAM buffer
-             ld    hl, cstMode     ; point to chest mode
-             ld    (hl), CHESTOFF  ; set chect mode to OFF
-             jp    +         ; forward to check column
-
-; Update chest sprite position.
-
-uptChest:    ld    a, (cstMode)
-             ld    c, a            ; chest mode
-             ld    d, (hl)         ; D
-             inc   hl
-             ld    e, (hl)         ; E
-             ld    b, CHESTSAT     ; B = Sprite index in SAT
-             call  goSprite        ; update SAT buffer (RAM)
-
-+: ;
 
 ; Scroll soldier if he is on screen.
 
--- sections/chest.asm
+++ sections/chest.asm
@@ -25,4 +25,44 @@ chkChest:    ld    a, (cstMode)    ; get chest mode
              ld    hl, cstMode     ; point to chest mode
              ld    (hl), CHESTOP   ; update mode to "open"
              ret
+
+; -------------------------------------------------------------------
+
+; Scroll chest if it is on screen.
+
+scrlCst:     ld   a, (cstMode)     ; point to chest mode
+             cp   CHESTOFF         ; is chest turned off?
+             ret   z               ; if so, skip to column check
+
+             ld   hl, cstX         ; point to chest x pos
+             dec  (hl)             ; decrement it
+             ld   a, (hl)          ; put value in A for a comparison
+             cp   0                ; is chest x = 0 (blanked clmn)?
+             jp   nz, +            ; if not, forward to update chest
+
+; Chest has scrolled off screen, so destroy it.
+
+             ld    c, 0            ; reset charcode
+             ld    d, 0            ; reset x pos
+             ld    e, 0            ; reset y pos
+             ld    b, CHESTSAT     ; B = the chest's index in SAT
+             call  goSprite        ; update SAT RAM buffer
+             ld    hl, cstMode     ; point to chest mode
+             ld    (hl), CHESTOFF  ; set chect mode to OFF
+             ret             ; forward to check column
+
+; Update chest sprite position.
+
++:           ld    a, (cstMode)
+             ld    c, a            ; chest mode
+             ld    d, (hl)         ; D
+             inc   hl
+             ld    e, (hl)         ; E
+             ld    b, CHESTSAT     ; B = Sprite index in SAT
+             call  goSprite        ; update SAT buffer (RAM)
+
+             ret ;
+
+
+
 .ends
