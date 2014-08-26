04b26bfa6671266e69b2d561308bf327edac9400
hang-on <asje@me.com>
2014-08-26 21:25:07 +0200

    Move soldier routine out of frame handler

--- sections/vblank.asm
+++ sections/vblank.asm
@@ -5,7 +5,7 @@
              exx                   ; save the rest of the registers
 
              call  scroller        ; scrolling the background
-             call  objects
+             ; call  objects
              call  updScore        ; write the digits
              call  hdlFrame        ; bluelib frame handler
              call  PSGFrame        ; psglib housekeeping
@@ -18,84 +18,6 @@
 .ends
 
 
-.section "Objects" free
-objects:
-
-; Respond to soldier's current mode (TODO: move out of VBlank!)
-
-             ld    a, (solMode)    ; get soldier mode
-             cp    SOLHURT         ; is he currently hurting?
-             jp    z, hdlHurt      ; handle hurting process
-
-             cp    SOLDYING        ; is he dying?
-             jp    nz, object2     ; if not, skip to next object
-
-; Soldier is dying - animate the sprite.
-
-             ld       hl, solCount ; param: cel counter
-             ld       de, solDying ; param: animation script
-             call     advcAnim     ; forward to net cel in animation
-
-             ld    hl, solDying    ; param: animation script
-             ld    a, (solCount)   ; param: freshly updated anim.
-             call  arrayItm        ; get charcode from anim. script
-             ld    c, a            ; put charcode in C (param)
-             ld    a, (solX)       ; get player's x position
-             ld    d, a            ; put it in D (param)
-             ld    a, (solY)       ; get player's y position
-             ld    e, a            ; put it in E (param)
-             ld    b, SOLSAT       ; B = plr sprite index in SAT
-             call  goSprite        ; update SAT buffer (RAM)
-
-             ld    a, (solCount)   ; get soldier's counter
-             cp    20              ; he is lying flat by now?
-             jp    nz, object2     ; if not, handle nwxt object
-             ld    hl, solMode     ; if so, point to soldier's mode
-             ld    (hl), SOLDEAD   ; and update it to "dead"
-
-; Soldier is dead; add to player's score.
-
-             ld    hl, score + 3   ; point to the hundreds column
-             ld    b,  2           ; one soldier is worth 200 points!
-             call  goScore         ; call the score updater routine
-             jp       object2      ; handle next object
-
-; Soldier is hurting (he is taking damage from player's weapon).
-
-hdlHurt:     ld    hl, solCount    ; point to soldier's counter
-             ld    a, (hl)         ; get value
-             cp    10              ; is this a new hurt sequence?
-             jp    nz, +           ; if not, skip forward...
-
-; A) New hurt sequence started - give soldier a yellow shirt.
-
-             ld    b, C7B2         ; soldier's shirt is col. 7, bnk 2
-             ld    c, YELLOW       ; set up for a yellow shirt
-             call  dfColor         ; define color in CRAM
-             jp    ++              ; skip forward to count down
-
-+:           cp    0               ; is counter = 0? (end hurt)
-             jp    nz, ++          ; if not, skip forward...
-
-; B) The hurting sequence has ended - give him his orange shirt back.
-
-             ld    b, C7B2         ; shirt is color 7 in CRAM bank 2
-             ld    c, ORANGE       ; prepare for an orange shirt
-             call  dfColor         ; define color in CRAM
-             ld    hl, solMode     ; point to soldier's mode variable
-             ld    (hl), SOLSTAND  ; switch back to standing
-             jp    object2         ; jump to next objects
-
-; C) Hurt sequence is just going on...
-
-++:          ld    hl, solCount
-             dec   (hl)            ; decrease counter
-
-; Handle next object (currently no other objects...)
-
-object2:
-             ret                   ; return to int. handler
-.ends
 
 
 .section "Update score display" free
