cb246cec11719a7b6a0de4516d8367820553960b
hang-on <asje@me.com>
2014-09-06 22:21:37 +0200

    Integrate VBlank module back into main

--- main.asm
+++ main.asm
@@ -138,12 +138,127 @@ gameLoop:
              jp    gameLoop        ; then over again...
 .ends
 
+.orga $0038
+.section "Maskable interrupt handler" force
+             ex    af, af'         ; save AF in their shadow register
+             in    a, VDPCOM       ; VDP status / satisfy interrupt
+             exx                   ; save the rest of the registers
+
+             call  scroller        ; scrolling the background
+             call  updScore        ; write the digits
+             call  hdlFrame        ; bluelib frame handler
+             call  PSGFrame        ; psglib housekeeping
+             call  PSGSFXFrame     ; process next SFX frame
+
+             exx                   ; restore the registers
+             ex    af, af'         ; also restore AF
+             ei                    ; enable interrupts
+             reti                  ; return from interrupt
+.ends
+
 
 
 .orga $0066
 .section "Non-Maskable interrupt handler (pause)" force
              retn                  ; disable pause button
 .ends
+
+.section "Update score display" free
+;TODO - make this a buffer to be otir'ed every frame...
+updScore:
+             ld    d, SCORE + 6    ; point to 100.000 digit (dest.)
+             ld    ix, score       ; point to score
+             ld    a, (ix + 0)     ; get MSB of score
+             sub   '0'             ; subtract ASCII zero
+             add   a, DIGITS       ; add tile bank index of digits
+             ld    e, a            ; put result in E (source)
+             call  putTile         ; write the digit tile
+             ld    a, (ix + 1)     ; next digit, 10.000
+             sub   '0'
+             add   a, DIGITS
+             inc   d
+             ld    e, a
+             call  putTile
+             ld    a, (ix + 2)     ; next digit, 1.000
+             sub   '0'
+             add   a, DIGITS
+             inc   d
+             ld    e, a
+             call  putTile
+             ld    a, (ix + 3)     ; next digit, 100
+             sub   '0'
+             add   a, DIGITS
+             inc   d
+             ld    e, a
+             call  putTile
+             ld    a, (ix + 4)     ; next digit, 10
+             sub   '0'
+             add   a, DIGITS
+             inc   d
+             ld    e, a
+             call  putTile
+             ld    a, (ix + 5)     ; next digit, 1
+             sub   '0'
+             add   a, DIGITS
+             inc   d
+             ld    e, a
+             call  putTile
+             ret
+.ends
+
+
+.section "Scroller handling" free
+scroller:
+; Every frame: Check scroller flag to see if screen needs scrolling.
+
+             ld    a, (scrlFlag)   ; read value of scroller flag
+             cp    1               ; is it set?
+             jp    nz, noScroll    ; if not, skip scrolling
+
+; Scroll the background/tilemap one pixel to the left.
+
+             ld    hl, scrlReg     ; update register value variable
+             dec   (hl)            ; scroll left = decrement
+             ld    a, (hl)         ; get current value (0-255)
+             out   (VDPCOM), a     ; send 1st byte of command word
+             ld    a, 8            ; register 8 = horiz. scroll
+             or    CMDREG          ; or with write register command
+             out   (VDPCOM), a     ; send 2nd byte of command word
+
+; The leftmost 8 pixels on screen hides (fully/partially) one column.
+; Check scroll value to see if next column is hidden = ready to fill.
+
+chkClmn:     ld    a, (scrlReg)    ; H. scroll reg. (#8) RAM mirror
+             and   %00000111       ; apply fine scroll mask
+             cp    0               ; next column completely hidden?
+             jp    nz, resFlag     ; if not > skip the following...
+
+; Update the hidden column in the name table .
+
+             ld    a, (nextClmn)   ; which clmn is currently hidden?
+             push  af
+;             call  setClmn         ; update it (minus status bar!)
+             call   LoadColumn
+             pop   af
+             inc   a
+             ld    (nextClmn), a   ; store next hidden column number
+
+
+
+; Reset the scroll flag and return.
+
+resFlag:     xor    a              ; clear A
+             ld    (scrlFlag), a   ; clear scroll flag
+
+; Finish scroll handler.
+
+noScroll:    ret                   ; return to frame int. handler
+
+
+.ends
+
+
+
 
 ; -------------------------------------------------------------------
 ;                            DATA
