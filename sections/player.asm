0e2ab99330071ec7fa5a6e216c4435abc55ffa9a
hang-on <asje@me.com>
2014-09-12 22:38:58 +0200

    Create _UpdatePlayerPosition

--- sections/player.asm
+++ sections/player.asm
@@ -78,6 +78,15 @@ ManagePlayerLoop:
              and   %11111110        ; turn of chest flag every loop
              ld    (player_flag), a
 
+             call   getPlr1
+             bit    CTBTN1, a
+             jp     nz, +
+             ld     a, (player_flag)
+             res    1, a
+             ld     (player_flag), a
++:
+
+
 
 ; Store the now expired player state as 'old state'.
 
@@ -103,28 +112,25 @@ ManagePlayerLoop:
 
              call  _HandleAttackingPlayer
 
+             call  _UpdatePlayerPosition
 
-; -------------------------------------------------------------------
-;                    (DON'T) SCROLL PLAYER                          ;
-; -------------------------------------------------------------------
-_step5:
+             ret
+
+
+_UpdatePlayerPosition:
+
+; Cancel horizontal movement if stage is scrolling.
+
              ld    a, (ScrollFlag)
              cp    1
-             jp    nz, _step6
+             jp    nz, +
              xor   a
              ld    (hSpeed), a     ; dont move forward.
 
-_step6:
 
-_step8:
-
-; -------------------------------------------------------------------
-;                           MOVE PLAYER / UPDATE POSITION           ;
-; -------------------------------------------------------------------
-_step10:
 ; Move player horizontally according to hSpeed.
 
-             ld    a, (hSpeed)     ; get horizontal speed
++:           ld    a, (hSpeed)     ; get horizontal speed
              ld    b, a            ; store it in B
              ld    a, (plrX)       ; get current x pos of player
              ld    (plrXOld), a    ; save it as old
@@ -144,64 +150,7 @@ _step10:
              xor   a
              ld   (vSpeed), a
 
-; -------------------------------------------------------------------
-;                    Check for collision w. chest                   ;
-; -------------------------------------------------------------------
-_step11:
-             ld    a, (ChestState)    ; get chest mode
-             cp    CHEST_IS_OFF        ; is it off/inactive?
-             jp    z, _step13      ; if no active chest skip coll.chk
-             ld    a, (plrState)
-             cp    WALK
-             jp    nz, _step13
-
-             ld    hl, plrX        ; point HL to player x,y data
-             ld    de, ChestX        ; point DE to chest x,y
-
-; NOTE: Implement new collision detection handler
-;             call  clDetect        ; call the collision detection sub
-             jp    nc, _step13              ; if no carry, then no collision
-
-; Check if chest is closed or open.
-
-             ld    a, (ChestState)    ; get chest mode
-             cp    CHEST_IS_CLOSED         ; is it closed?
-             jp    nz, +           ; if so, then player cannot pass!
-             call  stopPlr
-             jp    _step13
-+:
-; If chest is open, then pick it up.
-             ld    hl, player_flag
-             set   0, (hl)
-
-             xor   a
-             ld    (ChestX), a
-             ld    (ChestY), a
-
-             ld    c, 0            ; reset charcode
-             ld    d, 0            ; reset x pos
-             ld    e, 0            ; reset y pos
-             ld    b, CHESTSAT     ; B = the chest's index in SAT
-             call  goSprite        ; update SAT RAM buffer
-             ld    hl, ChestState     ; point to chest mode
-             ld    (hl), $ff       ; turn it off now
-
-             ld    hl,sfxBonus     ; point to bonus SFX
-             ld    c,SFX_CHANNELS2AND3  ; in chan. 2 and 3
-             call  PSGSFXPlay      ; play the super retro bonus sound
-
-
-
-_step13:
-             call   getPlr1
-             bit    CTBTN1, a
-             jp     nz, +
-             ld     a, (player_flag)
-             res    1, a
-             ld     (player_flag), a
-+:
              ret
-
 
 _GetInput:
 
@@ -409,6 +358,55 @@ ContinueAttack:
              ld    b, WPONSAT      ; B = the weapon's index in SAT
              call  goSprite        ; update SAT RAM buffer
              ret
+
+/*
+; -------------------------------------------------------------------
+;                    Check for collision w. chest                   ;
+; -------------------------------------------------------------------
+_step11:
+             ld    a, (ChestState)    ; get chest mode
+             cp    CHEST_IS_OFF        ; is it off/inactive?
+             jp    z, _step13      ; if no active chest skip coll.chk
+             ld    a, (plrState)
+             cp    WALK
+             jp    nz, _step13
+
+             ld    hl, plrX        ; point HL to player x,y data
+             ld    de, ChestX        ; point DE to chest x,y
+
+; NOTE: Implement new collision detection handler
+;             call  clDetect        ; call the collision detection sub
+             jp    nc, _step13              ; if no carry, then no collision
+
+; Check if chest is closed or open.
+
+             ld    a, (ChestState)    ; get chest mode
+             cp    CHEST_IS_CLOSED         ; is it closed?
+             jp    nz, +           ; if so, then player cannot pass!
+             call  stopPlr
+             jp    _step13
++:
+; If chest is open, then pick it up.
+             ld    hl, player_flag
+             set   0, (hl)
+
+             xor   a
+             ld    (ChestX), a
+             ld    (ChestY), a
+
+             ld    c, 0            ; reset charcode
+             ld    d, 0            ; reset x pos
+             ld    e, 0            ; reset y pos
+             ld    b, CHESTSAT     ; B = the chest's index in SAT
+             call  goSprite        ; update SAT RAM buffer
+             ld    hl, ChestState     ; point to chest mode
+             ld    (hl), $ff       ; turn it off now
+
+             ld    hl,sfxBonus     ; point to bonus SFX
+             ld    c,SFX_CHANNELS2AND3  ; in chan. 2 and 3
+             call  PSGSFXPlay      ; play the super retro bonus sound
+
+*/
 
 
 .ends
